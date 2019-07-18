8340a6da93b5d0b692bf5c129b0dfc85d8a7dda5
Ariela Cohen <arielaades@gmail.com>
2019-07-18 18:02:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ word.match?(/\b[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/(\b{un}.+{ing}\b)/)
+text.scan(/\b{un}.+{ing}\b/)
 end
 
 def words_five_letters_long(text)
-text.scan(/\A\w{5}\z/)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
