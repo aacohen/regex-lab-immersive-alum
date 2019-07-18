04ed5d6ec2196752157aa981f38f1d6ea36fef39
Ariela Cohen <arielaades@gmail.com>
2019-07-18 18:01:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ text.scan(/(\b{un}.+{ing}\b)/)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+text.scan(/\A\w{5}\z/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
